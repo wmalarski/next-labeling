7783b754ecabc84937205f27f60b19e370556b07
wojciech <w.malarski@gmail.com>
2020-09-20 12:55:31 +0200

    attribute change added to history

--- src/components/editors/editorTable.tsx
+++ src/components/editors/editorTable.tsx
@@ -1,33 +1,70 @@
+import Paper from "@material-ui/core/Paper";
 import React, { useContext } from "react";
+import FramesContext from "../../contexts/frames/framesContext";
 
 import LabelingContext from "../../contexts/labeling/labelingContext";
 import SelectionContext from "../../contexts/selection/selectionContext";
-import { LabelingField, LabelingObject } from "../../utils/labeling/types";
-import { FieldSchema, ObjectSchema } from "../../utils/schema/types";
+import { ExtendedField, ExtendedObject } from "../../utils/labeling/types";
+import { changeAttributeUpdate } from "../../utils/labeling/updates";
+import FieldEditor from "./fieldEditor";
 
 export interface TableObject {
-  object: LabelingObject;
-  objectSchema: ObjectSchema;
-  fields: {
-    field: LabelingField;
-    fieldSchema: FieldSchema;
-  };
+  object: ExtendedObject;
+  fields: ExtendedField[];
 }
 
 export default function EditorTable(): JSX.Element {
   const { history } = useContext(LabelingContext);
   const { selected } = useContext(SelectionContext);
+  const { currentFrame } = useContext(FramesContext);
 
   const { objects } = history.data;
 
-  selected.flatMap(selection => {
+  const tableObjects: TableObject[] = selected.flatMap(selection => {
     const object = objects.find(object => object.id === selection.objectId);
     if (!object) return [];
+    return [
+      {
+        object,
+        fields: selection.objectSelected
+          ? object.fields
+          : selection.fieldIds.flatMap(fieldId => {
+              const field = object.fields.find(f => f.id === fieldId);
+              return field ? [field] : [];
+            }),
+      },
+    ];
   });
 
   return (
-    <div>
-      <pre>{JSON.stringify(history.data, null, 2)}</pre>
-    </div>
+    <>
+      {tableObjects.map(({ object, fields }) => (
+        <Paper key={object.id}>
+          {fields.map(field => (
+            <FieldEditor
+              key={field.id}
+              attributes={field.fieldSchema.attributes}
+              disabled={object.isDone}
+              frame={currentFrame}
+              name={field.fieldSchema.name}
+              perFrame={field.fieldSchema.perFrame}
+              values={field.values}
+              onChange={provider => {
+                history.setLabeling(data => ({
+                  message: "Attribute value changed",
+                  data: changeAttributeUpdate(
+                    data,
+                    object,
+                    field,
+                    provider(field.values),
+                  ),
+                }));
+              }}
+            />
+          ))}
+        </Paper>
+      ))}
+      <pre>{JSON.stringify(tableObjects, null, 2)}</pre>
+    </>
   );
 }
--- src/utils/labeling/updates.ts
+++ src/utils/labeling/updates.ts
@@ -1,6 +1,7 @@
 import { createObject } from "./functions";
-import { ExtendedLabeling } from "./types";
+import { ExtendedField, ExtendedLabeling, ExtendedObject } from "./types";
 import { ObjectSchema } from "../schema/types";
+import { LabelingFieldValues } from "../editors/types";
 
 export function addObjectUpdate(
   data: ExtendedLabeling,
@@ -11,4 +12,21 @@ export function addObjectUpdate(
     ...data,
     objects: [...data.objects, createObject(objectSchema, currentFrame)],
   };
+}
+
+export function changeAttributeUpdate(
+  data: ExtendedLabeling,
+  object: ExtendedObject,
+  field: ExtendedField,
+  values: LabelingFieldValues,
+): ExtendedLabeling {
+  const fieldIndex = object.fields.findIndex(f => f.id === field.id);
+  const fields = [...object.fields];
+  fields[fieldIndex] = { ...field, values };
+
+  const objectIndex = data.objects.findIndex(obj => obj.id === object.id);
+  const objects = [...data.objects];
+  objects[objectIndex] = { ...object, fields };
+
+  return { objects };
 }
